9c34e7754a4783b1c8cc8269bf0ba0ce6b48b332
Athang17 <113341199+Athang17@users.noreply.github.com.>
2025-03-02 19:52:15 +0530

    Modified UserRepository file

--- UserService/UserService/src/main/java/com/example/UserService/repository/UserRepository.java
+++ UserService/UserService/src/main/java/com/example/UserService/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.example.UserService.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository extends JpaRepository<User, Long> {
 }
