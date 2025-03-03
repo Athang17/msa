ecbd49814e04159533799d1b22c178df578d4b16
Athang17 <113341199+Athang17@users.noreply.github.com.>
2025-03-03 14:10:10 +0530

    Modified service file

--- MovieService/src/main/java/com/example/MovieService/controller/MovieController.java
+++ MovieService/src/main/java/com/example/MovieService/controller/MovieController.java
@@ -1,4 +1,50 @@
 package com.example.MovieService.controller;
 
+import com.example.MovieService.entity.Movie;
+import com.example.MovieService.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/movies")
 public class MovieController {
+    @Autowired
+    private MovieService movieService;
+
+    @PostMapping
+    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
+        Movie createdMovie = movieService.createMovie(movie);
+        return ResponseEntity.ok(createdMovie);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return ResponseEntity.ok(movieService.getAllMovies());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable Long id) {
+        return movieService.getMovieById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie movieDetails) {
+        try {
+            Movie updatedMovie = movieService.updateMovie(id, movieDetails);
+            return ResponseEntity.ok(updatedMovie);
+        } catch (RuntimeException ex) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable Long id) {
+        movieService.deleteMovie(id);
+        return ResponseEntity.noContent().build();
+    }
 }
